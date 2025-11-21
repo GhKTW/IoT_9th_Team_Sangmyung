73b19b6c0040f36553dfd5cc16b2302c9a94418e
GhKTW <rlaxodn00100@naver.com>
2025-11-21 17:15:27 +0900

    Revert "feat-로드셀 측정 기능 구현"
    This reverts commit 535f6eecde1b184a8578584e6625d475676eb717.

--- sensors/__init__.py
+++ sensors/__init__.py
@@ -2,21 +2,17 @@ from .distanceAndLightlevel import init_spi, close_spi, get_distance_values, get
 from .lintracer import get_line_values
 from .light import lightOn, lightOff
 from .motor import leftMotorForward, leftMotorBackward, rightMotorForward, rightMotorBackward
-from .loadcell import setup_loadcell, read_weights, cleanup_loadcell
 
 __all__ = [
     "init_spi",
     "close_spi",
     "get_distance_values",
     "get_light_value",
-    "get_line_values",
+    "getLineValues",
     "lightOn",
     "lightOff",
     "leftMotorForward",
     "leftMotorBackward",
     "rightMotorForward",
-    "rightMotorBackward",
-    "setup_loadcell",
-    "read_weights",
-    "cleanup_loadcell"
+    "rightMotorBackward"
 ]
